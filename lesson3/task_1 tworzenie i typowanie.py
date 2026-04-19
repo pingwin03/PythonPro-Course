334a1ec7a38d9678e20e27bf808c27fbce6e5093
pingwin03 <pingwin03@gmail.com>
2026-04-19 18:21:28 +0200

    Add files via upload

---
+++ lesson3/task_1 tworzenie i typowanie.py
@@ -0,0 +1,10 @@
+imie = "Rafał"           # typ str (tekst)
+wiek = 42               # typ int (liczba całkowita)  
+srednia = 4.75           # typ float (liczba zmiennoprzecinkowa)
+student = True           # typ bool (wartość logiczna)
+
+# Wyświetlenie wartości i typów zmiennych
+print(f"Imię: {imie}, typ: {type(imie)}")
+print(f"Wiek: {wiek}, typ: {type(wiek)}")
+print(f"Średnia ocen: {srednia}, typ: {type(srednia)}")
+print(f"Student: {student}, typ: {type(student)}")
---
+++ lesson3/task_2 kalkulator BMI.py
@@ -0,0 +1,6 @@
+waga = float(input("Podaj swoją wagę w kilogramach: "))
+wzrost = float(input("Podaj swój wzrost w metrach: "))
+
+bmi = waga / (wzrost * wzrost)
+
+print(f"Twoje BMI wynosi: {bmi}")
