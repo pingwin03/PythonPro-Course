44bf4db5530f8c420fd16feca99173837b92c60a
pingwin03 <pingwin03@gmail.com>
2026-06-09 18:56:29 +0200

    Update task_6.py

--- lesson5/task_6.py
+++ lesson5/task_6.py
@@ -1,10 +1,14 @@
 sekret = 42
 
 while True:
-    liczba = int(input("Zgadnij liczbę: "))
+    try:
+        liczba = int(input("Podaj liczbę: "))
+    except ValueError:
+        print("Błąd: wpisz poprawną liczbę całkowitą.")
+        continue
 
     if liczba == sekret:
         print("Gratulacje! Odgadłeś liczbę!")
         break
     else:
-        print("To zła liczba, spróbuj ponownie.")
+        print("Zła liczba, spróbuj jeszcze raz.")
