c26cd2b0ef2dd89e41dc94525e4baca4554c65e9
pingwin03 <pingwin03@gmail.com>
2026-06-09 19:04:42 +0200

    Update task_7 niemutowalność krotki.py

--- lesson3/task_7 niemutowalność krotki.py
+++ lesson3/task_7 niemutowalność krotki.py
@@ -2,4 +2,10 @@ punkt = (10, 20, 30)
 
 # Próba zmiany elementu krotki spowoduje błąd,
 # ponieważ krotki są niemutowalne — nie można ich modyfikować po utworzeniu.
-punkt[0] = 15
+# punkt[0] = 15
+
+
+try:
+    punkt[0] = 15
+except TypeError:
+    print("Błąd: krotka jest niemutowalna, więc nie można zmienić jej elementu.")
